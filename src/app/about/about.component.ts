2b81f69c9583dcfbd5e576b9218e51ebb76e2de2
Vani Thilagar <vthilagar@parallelwireless.com>
2020-08-10 00:15:04 -0400

    Day 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { interval, timer, fromEvent, Observable, noop} from 'rxjs';
+import {map } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -10,6 +12,41 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //fetch('/api/courses');
+    const http$ = createHttpObservable('/api/courses')
+
+    const courses$ = http$
+    .pipe(
+      map(res => Object.values(res['payload'])),
+      
+    );
+
+    courses$.subscribe(
+      value => console.log("courses are ", value),
+      noop,
+      () => console.log('completed')
+    );
   }
 
+
+}
+
+
+
+
+
+function createHttpObservable(url: string) {
+ return Observable.create(observer => {
+      fetch(url)
+        .then(response => {
+          return response.json();
+        })
+        .then(body => {
+          observer.next(body);
+          observer.complete();
+        })
+        .catch(err => {
+          observer.error(err);
+        });
+    });
 }
